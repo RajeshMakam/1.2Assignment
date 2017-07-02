a0b0b0fc448866158d89ad928e85987251700828
Rajesh <Rajesh@DESKTOP-GAGEKBG>
2017-07-02 22:43:04 +0530

    Commit

--- src/addiionpack/Demo7.java
+++ src/addiionpack/Demo7.java
@@ -17,8 +17,6 @@ public class Demo7 {
           
             a = a ^b; //SUM of two bits is A XOR B
             
-            
-          
             b = carry << 1; //shifts carry to 1 bit to calculate sum
         }
          return a;
